4232a056dcc564987426e990e6acf96a382791e6
kevin <hemantsaroj694@gmail.com>
2024-03-02 20:45:50 +0530

    initial commit

---
+++ src/main/java/com/gitpractice/GitPractice/demo/Employee.java
@@ -0,0 +1,8 @@
+package com.gitpractice.GitPractice.demo;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/test/java/com/gitpractice/GitPractice/GitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitpractice.GitPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
